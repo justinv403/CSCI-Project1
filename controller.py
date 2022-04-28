a48d5571b47be01a1b70566d5b5965e514d6ff53
Justin <justinv403@gmail.com>
2022-04-27 23:49:20 -0500

    Switched from PyQt5 to Tkinter
    PyQt5 was refusing to work so I had to switch to Tkinter. PyQt kept reporting that a "native Qt signal is not callable" even after reviewing the video lectures and instructions from lab 10/test 10

--- controller.py
+++ controller.py
@@ -1,12 +1,12 @@
 from PyQt5.QtWidgets import *
-from gui import Ui_MainWindow
+from view import Ui_Remote
+import csv
 
-class Controller(QMainWindow, Ui_MainWindow):
+class Controller(QMainWindow, Ui_Remote):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
-        
         self.volumeDownButton.clicked(lambda : self.volumeDown())
 
     def volumeDown(self):
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from controller import *
+from gui import *
 
 def main():
-    app = QApplication([])
-    window = Controller()
-    window.show()
-    app.exec_()
+    """
+    - Change the window title to 'TV Remote'.
+    - Set its length to 300 and height to 600.
+    - Make the window non-resizable.
+    """
+    window = Tk()
+    widgets = GUI(window)
+    window.title("TV Remote")
+    window.geometry("300x600")
+    window.resizable(False,False)
+    window.mainloop()
+
 
 if __name__ == "__main__":
     main()
