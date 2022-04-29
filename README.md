592319cfe10e7fea92ad17ec28ec37a1b7773c80
Justin <justinv403@gmail.com>
2022-04-28 21:18:15 -0500

    Fixed error in last commit
    Fixed the error with the program that occurred after the last submit relating to the widgets in main.py

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CSCI-Project1
  Project #1 for CSCI-1620 - Built on the TV remote program from lab 9
 
+ 
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     Main function for the program - launches the GUI contained in gui.py
     """
     window = Tk()
+    widgets = GUI(window)
     window.title("TV Remote")
     window.geometry("300x700")
     window.resizable(False,False)
